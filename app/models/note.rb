c1163fcdf93baead63816b71caff6c58c7d6cb6d
Crystal <yxcwuu@gmail.com>
2020-02-20 11:16:48 -0500

    Adjust image size on show page

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
-    has_attached_file :image, styles: { large: "800x800>", medium: "300x300>", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
+    has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "150x150#" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     belongs_to :user
 end
